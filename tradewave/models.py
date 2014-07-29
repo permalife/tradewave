61b54a300a837d76d67dabe87d05230ec6c3d701
DMalone87 <darrell@endeavorms.com>
2014-07-28 21:40:13 -0500

    credit

--- tradewave/models.py
+++ tradewave/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Credit(models.Model):
-    producer = models.CharField(max_length=200)
+    producer = models.CharField(max_length=100)
     create_date = models.DateTimeField('date created')
 
