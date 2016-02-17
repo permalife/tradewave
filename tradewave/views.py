2819c8ec588373ecbcd7e017c9f0314c6c73c27d
Nickolay Shestopalov <nickolay@edyn.com>
2016-02-17 00:06:25 -1000

    Personal / Vendor / Marketplace session roles

--- tradewave/views.py
+++ tradewave/views.py
@@ -113,7 +113,7 @@ class SettingsMarketplace(generic.ListView):
     model = User
     template_name = 'tradewave/settings-marketplace.html'
 
-# *** handlers [process] ***
+# *** handler to process user login ***
 def process_login(request):
     try:
         # TO-DO: use django forms
@@ -129,14 +129,24 @@ def process_login(request):
             login(request, user)
             logger.info('Logged in as user [%s]', user.username)
             user_tradewave = TradewaveUser.objects.get(user=user.pk)
-
-            # sesssion-wide variable user role (default to personal):
-            request.session['user_role'] = 'personal'
-
             user_name = user.username
 
             # user's personal entity
             user_entity = user.tradewaveuser.user_entity
+
+            # session-wide variable user personal entity
+            request.session['entity_personal'] = user_entity.name
+            logger.info('personal entity name: %s', user_entity.name)
+
+            # session-wide variable vendor entity
+            if user_tradewave.vendors.exists():
+                request.session['entity_vendor'] = user_tradewave.vendors.first().name
+                logger.info('vendor entity name: %s', user_tradewave.vendors.first().name)
+
+            # session-wide variable user marketplace entity
+            if user_tradewave.marketplaces.exists():
+                request.session['entity_marketplace'] = user_tradewave.marketplaces.first().name
+                logger.info('marketplace entity name: %s', user_tradewave.marketplaces.first().name)
 
             # generate the list of credits
             # we limit to a single account for simplicity for now
@@ -144,38 +154,123 @@ def process_login(request):
             user_amount_total = user_account.amount_total
             user_wallet = CreditMap.objects.filter(account=user_account)
             user_credits = dict([
-                (entry.credit.name, entry.amount) for entry in user_wallet
+                (entry.credit.name, '%.2f' % entry.amount) for entry in user_wallet
             ])
 
             # generate the render link
             template_prefix = 'user'
             template_handle = 'tradewave/%s-home.html' % template_prefix
             logger.info('redirecting to %s', template_handle)
+
+            # standard request object to the user page
             request_obj = {
-                'featured_venues': Venue.objects.all()[:3],
-                'username': user_name,
-                'amount': user_amount_total,
+                #'featured_venues': Venue.objects.all()[:3],
+                'name': user_name,
+                'total': user_amount_total,
                 'credits': user_credits,
             }
+            request.session['name'] = user_name
+            request.session['total'] = '%.2f' % user_amount_total
+            request.session['credits'] = user_credits
 
             return render(request, template_handle, request_obj)
         else:
-            request_obj = { 'status_msg': 'Invalid credentials'}
+            request_obj = {'status_msg': 'Invalid login / password'}
             return render(request, 'tradewave/login.html', request_obj)
+
     except Exception as e:
         logger.error("Server error: %s", e)
-        request_obj = { 'status_msg': 'Error logging in: %s' % e }
+        request_obj = {
+            'status_msg': 'Server error occured, please contact the administrator'
+        }
         return render(request, 'tradewave/login.html', request_obj)
 
-# *** handlers [process] ***
+
+# *** handler to process user logout ***
 def process_logout(request):
     try:
         logout(request)
     except Exception as e:
         logger.error("Server error: %s", e)
     finally:
-        request_obj = { 'status_msg': 'Please login to your account'}
+        request_obj = {'status_msg': 'Please login to your account'}
         return render(request, 'tradewave/login.html', request_obj)
+
+
+# *** handler to redirect to the personal page ***
+def redirect_to_personal(request):
+    try:
+        request_obj = {
+            'name': request.session['entity_personal'],
+            'total': request.session['total'],
+            'credits': request.session['credits'],
+            #'featured_venues': Venue.objects.all()[:3]
+        }
+        return render(request, 'tradewave/user-home.html', request_obj)
+
+    except Exception as e:
+        logger.error("Server error: %s", e)
+        request_obj = {
+            'status_msg': 'Server error occurred: please contact the administrator'
+        }
+        return render(request, 'tradewave/login.html', request_obj)
+
+
+# *** handler to redirect to the vendor page, if applicable ***
+def redirect_to_vendor(request):
+    try:
+        if request.session.has_key('entity_vendor'):
+            logger.info('user has a vendor association')
+            request_obj = {
+                'name': request.session['entity_vendor'],
+                'featured_venues': Venue.objects.all()[:3],
+            }
+            return render(request, 'tradewave/vendor-initial.html', request_obj)
+        else:
+            logger.info('user has no vendor associations')
+            request_obj = {
+                'name': request.session['name'],
+                'total': request.session['total'],
+                'credits': request.session['credits'],
+                'status_msg': 'Your account is not associated with any vendor'
+            }
+            return render(request, 'tradewave/user-home.html', request_obj)
+
+    except Exception as e:
+        logger.error("Server error: %s", e)
+        request_obj = {
+            'status_msg': 'Server error occurred: please contact the administrator'
+        }
+        return render(request, 'tradewave/login.html', request_obj)
+
+
+# *** handler to redirect to the marketplace page, if applicable ***
+def redirect_to_marketplace(request):
+    try:
+        if request.session.has_key('entity_marketplace'):
+            logger.info('user has a marketplace association')
+            request_obj = {
+                'name': request.session['entity_marketplace'],
+                'featured_venues': Venue.objects.all()[:3]
+            }
+            return render(request, 'tradewave/marketplace-initial.html', request_obj)
+        else:
+            logger.info('user has no marketplace associations')
+            request_obj = {
+                'name': request.session['name'],
+                'total': request.session['total'],
+                'credits': request.session['credits'],
+                'status_msg': 'Your account is not associated with any marketplace'
+            }
+            return render(request, 'tradewave/user-home.html', request_obj)
+
+    except Exception as e:
+        logger.error("Server error: %s", e)
+        request_obj = {
+            'status_msg': 'Server error occurred: please contact the administrator'
+        }
+        return render(request, 'tradewave/login.html', request_obj)
+
 
 # *** handlers [record] ***
 def record_venue(request, venue_id):
