b492a07e90c27f63f94ff839f044596e45780ce1
Nickolay Shestopalov <nickolay@edyn.com>
2016-02-22 10:18:10 -1000

    Dashboard and account data export feature

--- tradewave/admin.py
+++ tradewave/admin.py
@@ -5,6 +5,29 @@ from tradewave.models import City, Venue, Entity, VenueMap, Credit, \
     Account, CreditMap, TradewaveUser, Relationship, Industry, Vendor, \
     Marketplace, Affiliation, TransactionLog
 
+from import_export import resources
+#from import_export.admin import ImportExportActionModelAdmin
+from import_export.admin import ImportExportModelAdmin
+
+
+class CreditMapResource(resources.ModelResource):
+
+    class Meta:
+        model = CreditMap
+        fields = (
+            'account__entity__name',
+            'credit__name',
+            'amount',
+            'account__date_last_transacted'
+        )
+        exclude = ('id',)
+
+
+class CreditMapAdmin(ImportExportModelAdmin):
+    resource_class = CreditMapResource
+    pass
+
+
 admin.site.register(City)
 admin.site.register(Entity)
 admin.site.register(Venue)
