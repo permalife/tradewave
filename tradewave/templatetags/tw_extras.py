8efbb9b04db8da01d4f6fea12b2354f79720bd44
Nickolay Shestopalov <nickolay@edyn.com>
2016-07-09 23:51:47 -1000

    Refactor

--- tradewave/templatetags/tw_extras.py
+++ tradewave/templatetags/tw_extras.py
@@ -15,4 +15,14 @@ def subtract(value, arg):
 @register.filter
 def subtractfirst(value, arg):
     #logger.info(arg.values)
-    return value - arg.values()[0]
+    return value - arg.values()[0]['amount']
+
+@register.filter
+def firstname(arg):
+    #logger.info(arg.values)
+    return arg.values()[0]['name']
+
+@register.filter
+def firstamount(arg):
+    #logger.info(arg.values)
+    return arg.values()[0]['amount']
