2ee761670a6e3b4568ebeb96a75b323ede2a945c
Nickolay Shestopalov <nickolay@edyn.com>
2016-11-07 20:09:47 -1000

    Add credit to product category mapping

--- tradewave/optimize/pick_credits.py
+++ tradewave/optimize/pick_credits.py
@@ -3,30 +3,36 @@ from scipy.optimize import minimize
 # number of produce items user is buying
 n = 2
 # prices of produce items
-A = [5, 9]
+A = [5, 19]
 
 # number of credit types
 m = 3
 # amount of credit types in user's wallet
-C = [4, 5, 6]
+C = [4, 5, 10]
+
+# specify which credits can buy which items
+# each row corresponds to a credit type
+# 1 means can buy item at index i, 0 means can not
+D = [[1, 0], [0, 1], [1, 1]]
 
 print 'Buying items: ' + str(A)
 print 'Wallet contents: ' + str(C)
+
+for i in xrange(m):
+    print 'Credit ' + str(i) + ' can buy: ' + str(D[i])
 
 # main cost function
 def f(x):
     f = 0
     for i in xrange(n):
-        s = 0
-        for j in xrange(m):
-            s += x[i*m + j]
+        s = sum(x[i*m : (i+1)*m])
         f += (A[i] - s)
     return f
 
 # define the constraints
 cons = []
 
-# pay requested amount (or less) for each item
+# try to pay requested amount for each item
 for i in xrange(n):
     cons.append({'type': 'ineq', 'fun': lambda x, i=i: A[i] - sum(x[i*m : (i+1)*m])})
 
@@ -39,7 +45,10 @@ bnds = []
 x0 = []
 for i in xrange(n):
     for j in xrange(m):
-        bnds.append((0, C[j]))
+        upper = 0
+        if D[j][i]:
+            upper = C[j]
+        bnds.append((0, upper))
         x0.append(0)
 
 # run the algorithm
