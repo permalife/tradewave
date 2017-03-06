65b06040a818061145d3e5fc223c95e50778eaa3
Nickolay Shestopalov <nickolay@edyn.com>
2017-03-05 21:50:21 -1000

    switch to production level

--- tradewave_beta/settings.py
+++ tradewave_beta/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x&vg@80e!c^x)(io_ut&w8@dkr(wk&okv^1$j_3+^mq+dtt!l*'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
